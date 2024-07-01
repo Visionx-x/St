7a7534f3d9bd4a28afb7484125530a9eeb28165e
Visionx-x <telegrambott19@gmail.com>
2024-07-01 12:19:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID","9333070"))
+API_HASH = getenv("API_HASH","511eb11eda4af78ec8f9a0a7de9e1241")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN","6505771827:AAHFZVhiKg6J1hmqljnsSbCduXBVDCSRQYA")
 OWNER_ID = int(getenv("OWNER_ID", "5016109398"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Public74:Public74@public74.ulukh3x.mongodb.net/?retryWrites=true&w=majority&appName=Public74")
+MUST_JOIN = getenv("MUST_JOIN","learningbots79")
