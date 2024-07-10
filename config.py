4a65f35032525a9adf7dc0a5e0bb5d6e130061df
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2024-07-10 16:37:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ API_HASH = getenv("API_HASH","511eb11eda4af78ec8f9a0a7de9e1241")
 BOT_TOKEN = getenv("BOT_TOKEN","6505771827:AAHFZVhiKg6J1hmqljnsSbCduXBVDCSRQYA")
 OWNER_ID = int(getenv("OWNER_ID", "5016109398"))
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Public74:Public74@public74.ulukh3x.mongodb.net/?retryWrites=true&w=majority&appName=Public74")
-MUST_JOIN = getenv("MUST_JOIN","learningbots79")
+MUST_JOIN = ["thanos_pro", "channel2"]
